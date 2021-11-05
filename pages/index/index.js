b027a73e09fed8e5e902bde821e92e12a755e0ab
bbyldebb <bb20011031@163.com>
2021-11-05 18:39:58 +0800

    Init.

---
+++ pages/index/index.js
@@ -0,0 +1,21 @@
+Page({
+  data: {
+    address: '地址地址'
+  },
+  onLoad() {
+    // 尝试连接数据库，似乎失败了
+    wx.cloud.database().collection('restraunt')
+      .get({
+        success(res) {
+          this.address = res[1].data.data.address
+          console.log(this.address)
+        },
+        fail(error) {}
+      })
+  },
+  toCustomer() {
+    wx.navigateTo({
+      url: '/pages/customer/customer',
+    })
+  }
+})
