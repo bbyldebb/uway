f42ffdf2b59ab81f3d4bae135fcedc818fb7e17c
bbyldebb <bb20011031@163.com>
2021-12-05 21:03:08 +0800

    顾客登录实现

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,3 +1,4 @@
+const CUSTOMER = wx.cloud.database().collection("customer")
 Page({
   data: {
     imgUrls: [
@@ -5,45 +6,73 @@ Page({
       'https://img.zcool.cn/community/0138265c626ab0a801213f268394eb.jpg@1280w_1l_2o_100sh.jpg',
       'https://www1.pchouse.com.cn/sheji/datu3.jpg'
     ],
-    showModal: false
+    showModal: false,
+    nickName: '',
+    avatarUrl: '',
+    openId: ''
   },
   toCustomer() {
-    var p1 = new Promise((resolve, reject) => {
-      wx.login({
-        success: res => {
-          resolve(res)
-        }
-      })
-    })
-    var p2 = new Promise((resolve, reject) => {
-      wx.getUserProfile({
-        desc: '用于完善会员资料',
-        success: res => {
-          resolve(res)
-        }
-      })
-    })
-    // 同时执行p1和p2，并在它们都完成后执行then
-    Promise.all([p1, p2]).then((results) => {
-      // results是一个长度为2的数组，放置着p1、p2的resolve
-      this.handleUserInfo({
-        // 这里也可以选择性返回需要的字段
-        ...results[0],
-        ...results[1]
-      })
+    var that = this;
+    //获取用户授权
+    wx.getUserProfile({
+      desc: '用于完善会员资料',
+      success: res => {
+        //获取用户的 nickName 和 avatarUrl
+        that.setData({
+          nickName: res.userInfo.nickName,
+          avatarUrl: res.userInfo.avatarUrl
+        })
+        //调用云函数获取用户的openId
+        wx.cloud.callFunction({
+          name: 'getOpenID',
+          complete: res => {
+            that.setData({
+              openId: res.result.openId
+            })
+            //查询用户的openId，若表中没有则添加记录
+            CUSTOMER.where({
+              openId: that.data.openId
+            }).get({
+              success: function (res) {
+                if (res.data.length == 0) {
+                  CUSTOMER.add({
+                    data: {
+                      avatarUrl: that.data.avatarUrl,
+                      nickName: that.data.nickName,
+                      openId: that.data.openId
+                    },
+                    success: function (res) {
+                      wx.showToast({
+                        title: '欢迎光临UWAY',
+                        icon: 'success',
+                        duration: 500
+                      });
+                      setTimeout(function () {
+                        wx.redirectTo({
+                          url: '/pages/customer/customer',
+                        })
+                      }, 500)
+                    }
+                  });
+                } else {
+                  wx.showToast({
+                    title: '欢迎老顾客回家',
+                    icon: 'success',
+                    duration: 500
+                  });
+                  setTimeout(function () {
+                    wx.redirectTo({
+                      url: '/pages/customer/customer',
+                    })
+                  }, 500)
+                }
+              }
+            });
+          }
+        })
+      }
     })
   },
-
-  // 组织好后端需要的字段，并调用接口
-  handleUserInfo(data) {
-    console.log(data);
-    console.log(data.code);
-    wx.redirectTo({
-      url: '/pages/customer/customer',
-    })
-
-  },
-
   toStaff() {
     this.setData({
       showModal: true
